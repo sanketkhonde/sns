22e164b07f5617878c909c8e3c240ec2c7bb163e
bash <sanketkhonde008@gmail.com>
2022-09-24 10:46:23 +0530

    rf

---
+++ src/practice/My.java
@@ -0,0 +1,10 @@
+package practice;
+
+class a{
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+}
---
+++ src/practice/aa.java
@@ -0,0 +1,10 @@
+package practice;
+
+import java.util.*;
+
+public class aa {
+
+	public static void main(String[] args) {
+
+
+	}}
